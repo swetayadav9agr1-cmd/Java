2e3c3a8a1aa1a85f99474e7a6097f3587f1549df
swetayadav9agr1-cmd <swetayadav9a.gr1@gmail.com>
2026-09-11 15:01:01 +0530

    Added age validation with custom exception

---
+++ predefinedException.java
@@ -0,0 +1,8 @@
+public class predefinedException {
+    public static void main(String[]args){
+        int []arr=[1,,2,3,4];
+        Scanner sc=new Scanner(System.in);
+        /'/'[]
+    }
+    
+}
