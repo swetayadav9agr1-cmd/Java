a78de95554f31a80a2182f3ff4212d2b6930cefd
Sweta Yadav <swetayadav9a.gr1@gmail.com>
2026-09-18 11:48:58 +0530

    Add files via upload

---
+++ StudentRegistration.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+public class StudentRegistration {
+
+    public static boolean registerStudent(Set<Integer> ids, int id) {
+        return ids.add(id);
+    }
+
+    public static boolean isRegistered(Set<Integer> ids, int id) {
+        return ids.contains(id);
+    }
+
+    public static int totalStudents(Set<Integer> ids) {
+        return ids.size();
+    }
+
+    public static void main(String[] args) {
+
+        Set<Integer> ids = new HashSet<>();
+
+        System.out.println(registerStudent(ids, 101));
+        System.out.println(registerStudent(ids, 102));
+        System.out.println(registerStudent(ids, 103));
+        System.out.println(registerStudent(ids, 101));
+
+        System.out.println("Registered: "
+                + isRegistered(ids, 102));
+
+        System.out.println("Total Students: "
+                + totalStudents(ids));
+    }
+}
