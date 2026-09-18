776b08d130252c9b4296d2e26d9b608d38c283ec
Sweta Yadav <swetayadav9a.gr1@gmail.com>
2026-09-18 12:02:38 +0530

    Add files via upload

---
+++ RemoveDuplicates.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+public class RemoveDuplicates {
+
+    public static Set<String> removeDuplicates(List<String> names) {
+        return new LinkedHashSet<>(names);
+    }
+
+    public static void main(String[] args) {
+
+        List<String> names = new ArrayList<>();
+
+        names.add("Aman");
+        names.add("Riya");
+        names.add("Karan");
+        names.add("Aman");
+        names.add("Riya");
+        names.add("Simran");
+        names.add("Karan");
+
+        Set<String> uniqueNames = removeDuplicates(names);
+
+        System.out.println("Original List: " + names);
+        System.out.println("Unique Names: " + uniqueNames);
+    }
+}
+
